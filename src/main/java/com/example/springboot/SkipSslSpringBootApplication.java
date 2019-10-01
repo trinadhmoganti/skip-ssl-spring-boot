9a67abe1c4ad5282ea6f00b185e4a0f65f639a31
Moganti <veera-venkata-trinadh-chandu.moganti@capgemini.com>
2019-09-30 21:27:16 -0400

    initial commit

---
+++ src/main/java/com/example/springboot/SkipSslSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipSslSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipSslSpringBootApplication.class, args);
+	}
+
+}
